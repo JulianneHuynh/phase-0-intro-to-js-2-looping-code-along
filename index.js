aecfea469c603eac4e6150c29c88431651668225
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-01 15:12:56 -0500

    commit tree d769711c9045f33bf2002b515f7ab99ce0bfb521

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// for loop : 
+// while 
+
+function writeCards(names, birthday){
+    const newWriteCards= []
+    for (let i = 0; i < names.length; i++){
+        console.log(`Thank you ${names[i]}`);
+        debugger;
+        newWriteCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newWriteCards;
+    
+}
+
+
+function countDown(startNumber){
+    while (startNumber >= 0 ) {
+        console.log(startNumber--);
+    }
+}
